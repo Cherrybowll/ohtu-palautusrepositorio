e97355113ce0ddc2373d524a37e239c1ff957395
Cherrybowl <81250492+Cherrybowll@users.noreply.github.com>
2024-11-11 23:52:04 +0200

    V2T13

--- viikko2/nhl-reader/src/index.py
+++ viikko2/nhl-reader/src/index.py
@@ -1,16 +1,25 @@
-import requests
-from player import Player
 from playerreader import PlayerReader
 from playerstats import PlayerStats
+import rich.prompt
+
+#Ei ehtinyt tehdä kunnon graafista käyttöliittymää. Lasketaanko silti? Toiminnallisuus löytyy.
 
 def main():
-    url = "https://studies.cs.helsinki.fi/nhlstats/2023-24/players"
+    seasons = ["2018-19", "2019-20", "2020-21", "2021-22", "2022-23", "2023-24", "2024-25"]
+    chosen_season = rich.prompt.Prompt.ask("Season:", choices=seasons)
+
+    url = f"https://studies.cs.helsinki.fi/nhlstats/{chosen_season}/players"
     reader = PlayerReader(url)
     stats = PlayerStats(reader)
-    players = stats.top_scorers_by_nationality("FIN")
 
-    for player in players:
-         print(player)
+    while True:
+        nationalities = stats.get_nationalities()
+        chosen_nationality = rich.prompt.Prompt.ask("Nationality:", choices=nationalities)
+
+        players = stats.top_scorers_by_nationality(chosen_nationality)
+
+        for player in players:
+            print(player)
 
 
 if __name__ == "__main__":
--- viikko2/nhl-reader/src/playerstats.py
+++ viikko2/nhl-reader/src/playerstats.py
@@ -4,4 +4,7 @@ class PlayerStats:
     
     def top_scorers_by_nationality(self, nationality):
         #What a beautiful one-liner amirite
-        return sorted(filter(lambda player: player.nationality==nationality, self.players), reverse=True, key=lambda player: player.goals+player.assists)
+        return sorted(filter(lambda player: player.nationality==nationality, self.players), reverse=True, key=lambda player: player.goals+player.assists)[:10]
+    
+    def get_nationalities(self):
+        return list(set([player.nationality for player in self.players]))
