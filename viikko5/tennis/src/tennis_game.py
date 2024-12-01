947e499f90070207e06857382e399552fa0fe312
Cherrybowl <81250492+Cherrybowll@users.noreply.github.com>
2024-12-01 22:44:34 +0200

    V5T3 replace weird loop for correct score calls with method calls

--- viikko5/tennis/src/tennis_game.py
+++ viikko5/tennis/src/tennis_game.py
@@ -36,20 +36,18 @@ class TennisGame:
             else:
                 score = f"Win for {self.player2_name}"
         else:
-            for i in range(1, 3):
-                if i == 1:
-                    temp_score = self.m_score1
-                else:
-                    score = score + "-"
-                    temp_score = self.m_score2
+            player1_score_call = self._score_to_call_before_deuce(self.m_score1)
+            player2_score_call = self._score_to_call_before_deuce(self.m_score2)
 
-                if temp_score == 0:
-                    score = score + "Love"
-                elif temp_score == 1:
-                    score = score + "Fifteen"
-                elif temp_score == 2:
-                    score = score + "Thirty"
-                elif temp_score == 3:
-                    score = score + "Forty"
+            score = f"{player1_score_call}-{player2_score_call}"
 
         return score
+
+    def _score_to_call_before_deuce(self, score):
+        score_to_call_dict = {
+            0: "Love",
+            1: "Fifteen",
+            2: "Thirty",
+            3: "Forty"
+            }
+        return score_to_call_dict[score] if score < 4 else None
