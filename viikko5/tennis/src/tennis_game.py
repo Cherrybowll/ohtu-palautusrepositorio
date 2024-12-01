68ed582555b883a5872756b686e010a386d66ffc
Cherrybowl <81250492+Cherrybowll@users.noreply.github.com>
2024-12-02 01:37:51 +0200

    V5T3 clearer naming and ordering

--- viikko5/tennis/src/tennis_game.py
+++ viikko5/tennis/src/tennis_game.py
@@ -15,6 +15,18 @@ class TennisGame:
         score = ""
         temp_score = 0
 
+        if self.m_score1 >= 4 or self.m_score2 >= 4:
+            score_diff = self._score_difference()
+
+            if score_diff == 1:
+                return f"Advantage {self.player1_name}"
+            if score_diff == -1:
+                return f"Advantage {self.player2_name}"
+            if score_diff > 1:
+                return f"Win for {self.player1_name}"
+            if score_diff < -1:
+                return f"Win for {self.player2_name}"
+
         player1_score_call = self._score_to_call_before_deuce(self.m_score1)
         player2_score_call = self._score_to_call_before_deuce(self.m_score2)
 
@@ -24,21 +36,8 @@ class TennisGame:
 
             return f"{player1_score_call}-All"
 
-        score = f"{player1_score_call}-{player2_score_call}"
+        return f"{player1_score_call}-{player2_score_call}"
 
-        if self.m_score1 >= 4 or self.m_score2 >= 4:
-            minus_result = self.m_score1 - self. m_score2
-
-            if minus_result == 1:
-                score = f"Advantage {self.player1_name}"
-            elif minus_result == -1:
-                score = f"Advantage {self.player2_name}"
-            elif minus_result >= 2:
-                score = f"Win for {self.player1_name}"
-            else:
-                score = f"Win for {self.player2_name}"
-
-        return score
 
     def _score_to_call_before_deuce(self, score):
         score_to_call_dict = {
