5a50432cb96491f70dc3c6bb5773e1531dbe4f63
Cherrybowl <81250492+Cherrybowll@users.noreply.github.com>
2024-12-17 02:59:56 +0200

    V7T3&4 final

--- viikko7/kivi-paperi-sakset/src/kivipaperisakset.py
+++ viikko7/kivi-paperi-sakset/src/kivipaperisakset.py
@@ -5,23 +5,24 @@ class KiviPaperiSakset:
     def pelaa(self):
         tuomari = Tuomari()
 
-        ekan_siirto = input("Ensimmäisen pelaajan siirto: ")
-        tokan_siirto = self._tokan_siirto(ekan_siirto)
-
-        print(f"Tietokone valitsi: {tokan_siirto}")
-
-        while self._onko_ok_siirto(ekan_siirto) and self._onko_ok_siirto(tokan_siirto):
-            tuomari.kirjaa_siirto(ekan_siirto, tokan_siirto)
-            print(tuomari)
-
-            ekan_siirto = input("Ensimmäisen pelaajan siirto: ")
+        while True:
+            ekan_siirto = self._ekan_siirto()
             tokan_siirto = self._tokan_siirto(ekan_siirto)
+
+            if self._onko_ok_siirto(ekan_siirto) and self._onko_ok_siirto(tokan_siirto):
+                tuomari.kirjaa_siirto(ekan_siirto, tokan_siirto)
+                print(tuomari)
+            else:
+                break
 
         print("Kiitos!")
         print(tuomari)
 
+    def _ekan_siirto(self):
+        return input("Ensimmäisen pelaajan siirto: ")
+
     def _tokan_siirto(self, ekan_siirto):
-        return input("Toisen pelaajan siirto: ")
+        raise Exception("Korvattava metodi")
 
     def _onko_ok_siirto(self, siirto):
         return siirto == "k" or siirto == "p" or siirto == "s"
--- viikko7/kivi-paperi-sakset/src/kps_tekoaly.py
+++ viikko7/kivi-paperi-sakset/src/kps_tekoaly.py
@@ -6,7 +6,8 @@ class KPSTekoaly(KiviPaperiSakset):
         self.tekoaly = tekoaly
 
     def _tokan_siirto(self, ekan_siirto):
-        self.tekoaly.aseta_siirto(ekan_siirto)
         siirto = self.tekoaly.anna_siirto()
         print(f"Tietokone valitsi: {siirto}")
+        self.tekoaly.aseta_siirto(ekan_siirto)
+
         return siirto
