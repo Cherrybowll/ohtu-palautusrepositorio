07360f06d1787f99655d14dbd53d4a13d75d5886
Cherrybowl <81250492+Cherrybowll@users.noreply.github.com>
2024-12-17 02:43:32 +0200

    V7T2&4

--- viikko7/kivi-paperi-sakset/src/kivipaperisakset.py
+++ viikko7/kivi-paperi-sakset/src/kivipaperisakset.py
@@ -1,14 +1,12 @@
 from tuomari import Tuomari
-from tekoaly_parannettu import TekoalyParannettu
 
 
-class KPSParempiTekoaly:
+class KiviPaperiSakset:
     def pelaa(self):
         tuomari = Tuomari()
-        tekoaly = TekoalyParannettu(10)
 
         ekan_siirto = input("Ensimmäisen pelaajan siirto: ")
-        tokan_siirto = tekoaly.anna_siirto()
+        tokan_siirto = self._tokan_siirto(ekan_siirto)
 
         print(f"Tietokone valitsi: {tokan_siirto}")
 
@@ -17,13 +15,15 @@ class KPSParempiTekoaly:
             print(tuomari)
 
             ekan_siirto = input("Ensimmäisen pelaajan siirto: ")
-            tokan_siirto = tekoaly.anna_siirto()
+            tokan_siirto = self._tokan_siirto(ekan_siirto)
 
             print(f"Tietokone valitsi: {tokan_siirto}")
-            tekoaly.aseta_siirto(ekan_siirto)
 
         print("Kiitos!")
         print(tuomari)
+
+    def _tokan_siirto(self, ekan_siirto):
+        raise Exception("Korvattava metodi")
 
     def _onko_ok_siirto(self, siirto):
         return siirto == "k" or siirto == "p" or siirto == "s"
